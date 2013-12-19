0eab18d33582b9f25f0c3e4f085b1a39a25d7739
kosyrkova <kosyrkova@5a6aabce-a326-4528-a5e9-8409bbc25adf>
2013-12-19 04:43:56 +0000

    FIX: Ticker class added to a repository.
    git-svn-id: https://ccfit.nsu.ru/svn/DT6/2013/group-12221/vmem/trunk@368 5a6aabce-a326-4528-a5e9-8409bbc25adf

---
+++ src/ru/nsu/ccfit/vmem/Ticker.java
@@ -0,0 +1,15 @@
+package ru.nsu.ccfit.vmem;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: manul
+ * Date: 12/19/13
+ * Time: 1:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ticker {
+    public static AtomicInteger ticker = new AtomicInteger();
+
+}
