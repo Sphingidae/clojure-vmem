5ef4e5f1159918df0a5c3a2337021d77e89672b4
kosyrkova <kosyrkova@5a6aabce-a326-4528-a5e9-8409bbc25adf>
2013-12-24 14:38:13 +0000

    FIX: Fixed argument convertion in merge conflict dispatchers.
    git-svn-id: https://ccfit.nsu.ru/svn/DT6/2013/group-12221/vmem/trunk@441 5a6aabce-a326-4528-a5e9-8409bbc25adf

--- src/clojure/ru/nsu/ccfit/vmem/clojure/user_api.clj
+++ src/clojure/ru/nsu/ccfit/vmem/clojure/user_api.clj
@@ -13,25 +13,28 @@
    join-set-user-added-removed-conflict
    join-set-user-addrem-addrem-conflict]
 
-  (fn [[elems1 added1 removed1] [elems2 added2 removed2]]
+  (fn [committed pending]
     "Analyze conflicts between two version sets and choose the right strategy"
-    (cond
-      (and (empty? removed1) added1 (empty? removed2) (empty? added2))
-      (join-set-user-added-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and removed1 (empty? added1) (empty? removed2) (empty? added2))
-      (join-set-user-removed-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and removed1 added1 (empty? removed2) (empty? added2))
-      (join-set-user-addrem-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and (empty? removed1) added1 (empty? removed2) added2)
-      (join-set-user-added-added-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and removed1 (empty? added1) removed2 (empty? added2))
-      (join-set-user-removed-removed-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and (empty? removed1) added1 removed2 (empty? added2))
-      (join-set-user-added-removed-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      (and removed1 added1 removed2 added2)
-      (join-set-user-addrem-addrem-conflict [elems1 added1 removed1] [elems2 added2 removed2])
-      ;;if nothing equal, let's switch the arguments
-      :else (join-set-conflict-analyzer [elems2 added2 removed2] [elems1 added1 removed1]))))
+    (let [[elems1 added1 removed1] (rev-to-vset committed)
+          [elems2 added2 removed2] (rev-to-vset pending)]
+      (cond
+        (and (empty? removed1) added1 (empty? removed2) (empty? added2))
+        (join-set-user-added-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and removed1 (empty? added1) (empty? removed2) (empty? added2))
+        (join-set-user-removed-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and removed1 added1 (empty? removed2) (empty? added2))
+        (join-set-user-addrem-none-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and (empty? removed1) added1 (empty? removed2) added2)
+        (join-set-user-added-added-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and removed1 (empty? added1) removed2 (empty? added2))
+        (join-set-user-removed-removed-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and (empty? removed1) added1 removed2 (empty? added2))
+        (join-set-user-added-removed-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        (and removed1 added1 removed2 added2)
+        (join-set-user-addrem-addrem-conflict [elems1 added1 removed1] [elems2 added2 removed2])
+        ;;if nothing equal, let's switch the arguments
+        :else (recur pending committed)))
+    ))
 
 
 ;;;Automatic conflict strategy analyzer for stacks
@@ -44,25 +47,28 @@
    join-stack-user-poped-poped-conflict
    join-stack-user-pushpop-pushpop-conflict]
 
-  (fn [[elems1 pushed1 popnum1] [elems2 pushed2 popnum2]]
+  (fn [committed pending]
     "Analyze conflicts between two version stacks and choose the right strategy"
-    (cond
-      (and (= popnum1 0) pushed1 (= popnum2 0) (empty? pushed2))
-      (join-stack-user-pushed-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (> popnum1 0) (empty? pushed1) (= popnum2 0) (empty? pushed2))
-      (join-stack-user-poped-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (> popnum1 0) pushed1 (= popnum2 0) (empty? pushed2))
-      (join-stack-user-pushpop-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (= popnum1 0) pushed1 (= popnum2 0) pushed2)
-      (join-stack-user-pushed-pushed-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (> popnum1 0) (empty? pushed1) (> popnum2 0) (empty? pushed2))
-      (join-stack-user-poped-poped-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (= popnum1 0) pushed1 (> popnum2 0) (empty? pushed2))
-      (join-stack-user-pushed-poped-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      (and (> popnum1 0) pushed1 (> popnum2 0) pushed2)
-      (join-stack-user-pushpop-pushpop-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
-      ;;if nothing equal, let's switch the arguments
-      :else (join-stack-conflict-analyzer [elems1 pushed1 popnum1] [elems2 pushed2 popnum2]))))
+    (let [[elems1 added1 removed1] (rev-to-vset committed)
+          [elems2 added2 removed2] (rev-to-vset pending)]
+      (cond
+        (and (= popnum1 0) pushed1 (= popnum2 0) (empty? pushed2))
+        (join-stack-user-pushed-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (> popnum1 0) (empty? pushed1) (= popnum2 0) (empty? pushed2))
+        (join-stack-user-poped-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (> popnum1 0) pushed1 (= popnum2 0) (empty? pushed2))
+        (join-stack-user-pushpop-none-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (= popnum1 0) pushed1 (= popnum2 0) pushed2)
+        (join-stack-user-pushed-pushed-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (> popnum1 0) (empty? pushed1) (> popnum2 0) (empty? pushed2))
+        (join-stack-user-poped-poped-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (= popnum1 0) pushed1 (> popnum2 0) (empty? pushed2))
+        (join-stack-user-pushed-poped-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        (and (> popnum1 0) pushed1 (> popnum2 0) pushed2)
+        (join-stack-user-pushpop-pushpop-conflict [elems1 pushed1 popnum1] [elems2 pushed2 popnum2])
+        ;;if nothing equal, let's switch the arguments
+        :else (recur pending committed)))
+    ))
 
 
 
@@ -77,24 +83,27 @@
    join-queue-user-added-removed-conflict
    join-queue-user-addrem-addrem-conflict]
 
-  (fn[[elems1 added1 remnum1] [elems2 added2 remnum2]]
+  (fn[committed pending]
     "Analyze conflicts between two version queues and choose the right strategy"
-    (cond
-      (and (= remnum1 0) added1 (= remnum2 0) (empty? added2))
-      (join-queue-user-added-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (> remnum1 0) (empty? added1) (= remnum2 0) (empty? added2))
-      (join-queue-user-removed-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (> remnum1 0) added1 (= remnum2 0) (empty? added2))
-      (join-queue-user-addrem-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (= remnum1 0) added1 (= remnum2 0) added2)
-      (join-queue-user-added-added-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (> remnum1 0) (empty? added1) (> remnum2 0) (empty? added2))
-      (join-queue-user-removed-removed-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (= remnum1 0) added1 (> remnum2 0) (empty? added2))
-      (join-queue-user-added-removed-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      (and (> remnum1 0) added1 (> remnum2 0) added2)
-      (join-queue-user-addrem-addrem-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
-      ;;if nothing equal, let's switch the arguments
-      :else (join-queue-conflict-analyzer [elems2 added2 remnum2] [elems1 added1 remnum1]))))
+    (let [[elems1 added1 removed1] (rev-to-vset committed)
+          [elems2 added2 removed2] (rev-to-vset pending)]
+      (cond
+        (and (= remnum1 0) added1 (= remnum2 0) (empty? added2))
+        (join-queue-user-added-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (> remnum1 0) (empty? added1) (= remnum2 0) (empty? added2))
+        (join-queue-user-removed-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (> remnum1 0) added1 (= remnum2 0) (empty? added2))
+        (join-queue-user-addrem-none-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (= remnum1 0) added1 (= remnum2 0) added2)
+        (join-queue-user-added-added-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (> remnum1 0) (empty? added1) (> remnum2 0) (empty? added2))
+        (join-queue-user-removed-removed-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (= remnum1 0) added1 (> remnum2 0) (empty? added2))
+        (join-queue-user-added-removed-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        (and (> remnum1 0) added1 (> remnum2 0) added2)
+        (join-queue-user-addrem-addrem-conflict [elems1 added1 remnum1] [elems2 added2 remnum2])
+        ;;if nothing equal, let's switch the arguments
+        :else (recur pending committed)))
+    ))
 
 
